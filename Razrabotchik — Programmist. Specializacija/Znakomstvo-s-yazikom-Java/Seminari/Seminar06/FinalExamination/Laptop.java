42e87ff697173265e16fcc6e7811e177130fde79
Azer Agazade <82523317+AgazadeAV@users.noreply.github.com>
2024-07-20 14:58:29 +0400

    fixed Laptop.java

--- Razrabotchik — Programmist. Specializacija/Znakomstvo-s-yazikom-Java/Seminari/Seminar06/FinalExamination/Laptop.java
+++ Razrabotchik — Programmist. Specializacija/Znakomstvo-s-yazikom-Java/Seminari/Seminar06/FinalExamination/Laptop.java
@@ -1,11 +1,11 @@
 package Seminari.Seminar06.FinalExamination;
 
 public class Laptop {
-    String model;
-    int ram;
-    int hdd;
-    String os;
-    String color;
+    private String model;
+    private int ram;
+    private int hdd;
+    private String os;
+    private String color;
 
     // Конструктор класса
     public Laptop(String model, int ram, int hdd, String os, String color) {
@@ -13,6 +13,46 @@ public class Laptop {
         this.ram = ram;
         this.hdd = hdd;
         this.os = os;
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public int getHdd() {
+        return hdd;
+    }
+
+    public void setHdd(int hdd) {
+        this.hdd = hdd;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
         this.color = color;
     }
 
